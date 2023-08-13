8fb0c969b1fb7dbe1a9af6b3315e62c80a642c4c
damon <jr.way123@gmail.com>
2023-08-13 22:02:37 +0800

    cropPDF-已解决左右问题，先左列再上下；
    代解决问题，由从下往上变成从上往下；
    需要注意，此时的cropbox变成左上角+右下角的组合；

--- cropPdf.py
+++ cropPdf.py
@@ -20,42 +20,24 @@ height = (y2 - y1) / 3
 labelID = 1
 
 
-for pages in range(num_pages):
+# for pages in range(num_pages):
+for cols in range(2):
     for rows in range(3):
-        for cols in range(2):
-            label_x1 = x1 + cols * width
-            label_y1 = y1 + rows * height
-            label_x2 = label_x1 + width
-            label_y2 = label_y1 + height
+        label_x1 = x1 + cols * width
+        label_y1 = y1 + rows * height
+        label_x2 = label_x1 + width
+        label_y2 = label_y1 + height
 
-            page.cropbox.lower_left = (label_x1, label_y1 + 20)
-            page.cropbox.upper_right = (label_x2, label_y2 + 20)
+        page.cropbox.lower_left = (label_x1, label_y1 + 20)
+        page.cropbox.upper_right = (label_x2, label_y2 + 20)
 
-            outFile = os.path.join(outDir, r'{package}_P{page}_L{label}.pdf'.format(package='package-FBA1791SY190', page=pageNo, label=labelID))
-            labelID += 1
+        outFile = os.path.join(outDir, r'{package}_P{page}_L{label}.pdf'.format(package='package-FBA1791SY190', page=pageNo, label=labelID))
+        labelID += 1
 
-            with open(outFile, 'wb') as output_pdf:
-                pdf_writer = PdfWriter()
-                pdf_writer.add_page(page)
-                pdf_writer.write(output_pdf)
-    pageNo += 1
-# # cropbox page into 2 colums and 3 rows
-# x1 = page.cropbox.lower_left[0]
-# x2 = page.cropbox.lower_right[0]
-# y1 = page.cropbox.lower_left[1]
-# y2 = page.cropbox.upper_left[1]
-#
-# width = (x2 - x1) / 2
-# height = (y2 - y1) / 3
-#
-#
-# labelID = 0
-#
-# # crop each into single page
-# for rows in range(3):
-#     for cols in range(2):
-#         label_x1 = x1 + cols * width
-#         label_y1 = y1 + rows * height
-#         label_x2 = label_x1 + width
-#         label_y2 = label_y1 + height
-# #
+        with open(outFile, 'wb') as output_pdf:
+            pdf_writer = PdfWriter()
+            pdf_writer.add_page(page)
+            pdf_writer.write(output_pdf)
+    # pageNo += 1
+
+print('PDF 按标签拆分, Done')
